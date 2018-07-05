31523c2bf6bf0dfbf7391bac181d25283b73f94b
Sander Jetten <sanderjetten@gmail.com>
2018-07-05 21:04:38 +0200

    Added webpack bundler

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+import webpack from 'webpack';
+import path from 'path';
+
+const env = process.env.NODE_ENV;
+
+export default {
+  mode: env || 'development',
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+        debug: true,
+        noInfo: false,
+      })
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    ]
+  }
+}
